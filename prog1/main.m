c7f671de7e7baafd42e39c7b98d23f3a769a28c9
高星 <gaoxing@gaos-macbook-pro.local>
2018-05-14 17:46:55 +0800

    define several vars in one line

--- prog1/main.m
+++ prog1/main.m
@@ -10,10 +10,12 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int sum;
+        int value2, value1, sum;
         
+        value1 = 50;
+        value2 = 25;
+        sum = value1 + value2;
         
-        sum = 50 + 25;
         NSLog(@"The sum of 50 and 25 is %i", sum);
     }
     return 0;
