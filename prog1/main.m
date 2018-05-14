025900f85f78981d2d1e5fbb6494a0979bd84d02
高星 <gaoxing@gaos-macbook-pro.local>
2018-05-14 14:33:12 +0800

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Gao Xing on 2018/5/14.
+//  Copyright © 2018年 Gao Xing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
