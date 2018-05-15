813a2466fcc9e586848fc1d06a6980d50d8ef43b
高星 <gaoxing@gaos-macbook-pro.local>
2018-05-15 10:25:56 +0800

    code everything in one file

--- prog1/main.m
+++ prog1/main.m
@@ -8,15 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
+//---- @interface section ----
+
+@interface Fraction: NSObject
+
+- (void) print;
+- (void) setNumerator: (int) n;
+- (void) setDenominator: (int) d;
+
+@end
+
+//---- @implementation section ----
+
+@implementation Fraction {
+    int numerator;
+    int denominator;
+}
+
+- (void) print {
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+- (void) setNumerator:(int) n {
+    numerator = n;
+}
+
+- (void) setDenominator:(int) d {
+    denominator = d;
+}
+
+@end
+
+//---- program section ----
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int value2, value1, sum;
+        Fraction *myFraction;
         
-        value1 = 50;
-        value2 = 25;
-        sum = value1 + value2;
+        // Create an instance of a Fraction
         
-        NSLog(@"The sum of 50 and 25 is %i", sum);
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        
+        // Set fraction to 1/3
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        
+        // Display the fraction using the print method
+        
+        NSLog(@"The value of myFraction is:");
+        [myFraction print];
     }
     return 0;
 }
